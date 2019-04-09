d14271fa02785e118301741209a620e9d0a896e8
Yatzii <47720837+5man999@users.noreply.github.com>
2019-04-09 17:41:39 +0100

    Update npc_professions.cpp

--- src/game/AI/ScriptDevAI/scripts/world/npc_professions.cpp
+++ src/game/AI/ScriptDevAI/scripts/world/npc_professions.cpp
@@ -178,6 +178,146 @@
 #define Q_MASTER_ELIXIR         10902
 #define Q_MASTER_POTION         10897
 
+#define GOSSIP_CAST_FORTITUDE         "I wish to have the Fortitude buff"
+#define GOSSIP_CAST_WISDOM       "I wish to have the Wisdom buff"
+#define GOSSIP_CAST_MIGHT      "I wish to have the Might buff"
+#define GOSSIP_CAST_SALVATION    "I wish to have the Salvation buff"
+#define GOSSIP_CAST_KINGS       "I wish to have the Kings buff"
+#define GOSSIP_CAST_ARCANE       "I wish to have the Arcane Intellect buff"
+#define GOSSIP_CAST_MARKOFTHEWILD       "I wish to have the Mark of The Wild buff"
+#define GOSSIP_CAST_SPIRIT       "I wish to have the Spirit buff"
+
+ /*###
+ # spells defines
+ ###*/
+
+#define S_FORTITUDE             21564
+#define S_WISDOM                25918
+#define S_MIGHT               	25916
+#define S_SALVATION             25895
+#define S_KINGS                 25898
+#define S_ARCANE              	23028
+#define S_MARKOFTHEWILD         21850
+#define S_SPIRIT                27681
+
+ /* Buff NPC */
+#define N_BUFFNPC     60003 
+
+/*###
+# formulas to calculate buff cost
+###*/
+
+bool GossipHello_npc_buffs(Player* pPlayer, Creature* pCreature)
+{
+	if (pCreature->isQuestGiver())
+		pPlayer->PrepareQuestMenu(pCreature->GetObjectGuid());
+	if (pCreature->isVendor())
+		pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_VENDOR, GOSSIP_TEXT_BROWSE_GOODS, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_TRADE);
+	if (pCreature->isTrainer())
+		pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_TRAINER, GOSSIP_TEXT_TRAIN, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_TRAIN);
+	uint32 eCreature = pCreature->GetEntry();
+
+	if (pPlayer->getLevel() == 70)
+	{
+		switch (eCreature)
+		{
+		case N_BUFFNPC:
+			pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, GOSSIP_CAST_FORTITUDE, GOSSIP_SENDER_LEARN, GOSSIP_ACTION_INFO_DEF + 1);
+			pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, GOSSIP_CAST_WISDOM, GOSSIP_SENDER_LEARN, GOSSIP_ACTION_INFO_DEF + 2);
+			pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, GOSSIP_CAST_MIGHT, GOSSIP_SENDER_LEARN, GOSSIP_ACTION_INFO_DEF + 3);
+			pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, GOSSIP_CAST_SALVATION, GOSSIP_SENDER_LEARN, GOSSIP_ACTION_INFO_DEF + 4);
+			pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, GOSSIP_CAST_KINGS, GOSSIP_SENDER_LEARN, GOSSIP_ACTION_INFO_DEF + 5);
+			pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, GOSSIP_CAST_ARCANE, GOSSIP_SENDER_LEARN, GOSSIP_ACTION_INFO_DEF + 6);
+			pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, GOSSIP_CAST_MARKOFTHEWILD, GOSSIP_SENDER_LEARN, GOSSIP_ACTION_INFO_DEF + 7);
+			pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, GOSSIP_CAST_SPIRIT, GOSSIP_SENDER_LEARN, GOSSIP_ACTION_INFO_DEF + 8);
+			break;
+		}
+	}
+	pPlayer->SEND_GOSSIP_MENU(pPlayer->GetGossipTextId(pCreature), pCreature->GetObjectGuid());
+	return true;
+}
+
+void SendActionMenu_npc_buffs(Player* pPlayer, Creature* pCreature, uint32 uiAction)
+{
+	switch (uiAction)
+	{
+	case GOSSIP_ACTION_TRADE:
+		pPlayer->SEND_VENDORLIST(pCreature->GetObjectGuid());
+		break;
+	case GOSSIP_ACTION_TRAIN:
+		pPlayer->SEND_TRAINERLIST(pCreature->GetObjectGuid());
+		break;
+		//Casted spells
+	case GOSSIP_ACTION_INFO_DEF + 1:
+		pPlayer->CastSpell(pPlayer, S_FORTITUDE, TRIGGERED_OLD_TRIGGERED);
+		pPlayer->CLOSE_GOSSIP_MENU();
+		break;
+	case GOSSIP_ACTION_INFO_DEF + 2:
+		pPlayer->CastSpell(pPlayer, S_WISDOM, TRIGGERED_OLD_TRIGGERED);
+		pPlayer->CLOSE_GOSSIP_MENU();
+		break;
+	case GOSSIP_ACTION_INFO_DEF + 3:
+		pPlayer->CastSpell(pPlayer, S_MIGHT, TRIGGERED_OLD_TRIGGERED);
+		pPlayer->CLOSE_GOSSIP_MENU();
+		break;
+	case GOSSIP_ACTION_INFO_DEF + 4:
+		pPlayer->CastSpell(pPlayer, S_SALVATION, TRIGGERED_OLD_TRIGGERED);
+		pPlayer->CLOSE_GOSSIP_MENU();
+		break;
+	case GOSSIP_ACTION_INFO_DEF + 5:
+		pPlayer->CastSpell(pPlayer, S_KINGS, TRIGGERED_OLD_TRIGGERED);
+		pPlayer->CLOSE_GOSSIP_MENU();
+		break;
+	case GOSSIP_ACTION_INFO_DEF + 6:
+		pPlayer->CastSpell(pPlayer, S_ARCANE, TRIGGERED_OLD_TRIGGERED);
+		pPlayer->CLOSE_GOSSIP_MENU();
+		break;
+	case GOSSIP_ACTION_INFO_DEF + 7:
+		pPlayer->CastSpell(pPlayer, S_MARKOFTHEWILD, TRIGGERED_OLD_TRIGGERED);
+		pPlayer->CLOSE_GOSSIP_MENU();
+		break;
+	case GOSSIP_ACTION_INFO_DEF + 8:
+		pPlayer->CastSpell(pPlayer, S_SPIRIT, TRIGGERED_OLD_TRIGGERED);
+		pPlayer->CLOSE_GOSSIP_MENU();
+		break;
+	}
+}
+
+void SendConfirmLearn_npc_buffs(Player* pPlayer, Creature* pCreature, uint32 uiAction)
+{
+	if (uiAction)
+	{
+		//uint32 eCreature = pCreature->GetEntry();
+		switch (pCreature->GetEntry())
+		{
+		case N_BUFFNPC:
+			pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, GOSSIP_CAST_FORTITUDE, GOSSIP_SENDER_CHECK, uiAction);
+			pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, GOSSIP_CAST_WISDOM, GOSSIP_SENDER_CHECK, uiAction);
+			pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, GOSSIP_CAST_MIGHT, GOSSIP_SENDER_CHECK, uiAction);
+			pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, GOSSIP_CAST_SALVATION, GOSSIP_SENDER_CHECK, uiAction);
+			pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, GOSSIP_CAST_KINGS, GOSSIP_SENDER_CHECK, uiAction);
+			pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, GOSSIP_CAST_ARCANE, GOSSIP_SENDER_CHECK, uiAction);
+			pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, GOSSIP_CAST_MARKOFTHEWILD, GOSSIP_SENDER_CHECK, uiAction);
+			pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, GOSSIP_CAST_SPIRIT, GOSSIP_SENDER_CHECK, uiAction);
+			//unknown textID ()
+			pPlayer->SEND_GOSSIP_MENU(pPlayer->GetGossipTextId(pCreature), pCreature->GetObjectGuid());
+			break;
+		}
+	}
+}
+
+
+bool GossipSelect_npc_buffs(Player* pPlayer, Creature* pCreature, uint32 uiSender, uint32 uiAction)
+{
+	switch (uiSender)
+	{
+	case GOSSIP_SENDER_MAIN:    SendActionMenu_npc_buffs(pPlayer, pCreature, uiAction); break;
+	case GOSSIP_SENDER_LEARN:   SendConfirmLearn_npc_buffs(pPlayer, pCreature, uiAction); break;
+	case GOSSIP_SENDER_CHECK:   SendActionMenu_npc_buffs(pPlayer, pCreature, uiAction); break;
+	}
+	return true;
+}
+
 
 /*###
 # formulas to calculate unlearning cost
@@ -1009,6 +1149,14 @@ void AddSC_npc_professions()
 	pNewScript->pGossipHello = &GossipHello_npc_prof_Alchemy;
 	pNewScript->pGossipSelect = &GossipSelect_npc_prof_Alchemy;
 	pNewScript->RegisterSelf();
+
+	pNewScript = new Script;
+	pNewScript->Name = "npc_buffs";
+	pNewScript->pGossipHello = &GossipHello_npc_buffs;
+	pNewScript->pGossipSelect = &GossipSelect_npc_buffs;
+	pNewScript->RegisterSelf();
+
+	
 
 	/*pNewScript = new Script;
 	pNewScript->Name = "go_soothsaying_for_dummies";
