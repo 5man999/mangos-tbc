83effdd50436b63939331b3e99328267636781a6
Yatzii <47720837+Yatzii93@users.noreply.github.com>
2019-04-21 19:06:59 +0100

    Update DuelHandler.cpp

--- src/game/Combat/DuelHandler.cpp
+++ src/game/Combat/DuelHandler.cpp
@@ -21,73 +21,101 @@
 #include "Server/WorldSession.h"
 #include "Log.h"
 #include "Entities/Player.h"
+#include "World/World.h"
 
 void WorldSession::HandleDuelAcceptedOpcode(WorldPacket& recvPacket)
 {
-    ObjectGuid guid;
-    recvPacket >> guid;
+	ObjectGuid guid;
+	recvPacket >> guid;
 
-    // Check for own duel info first
-    Player* self = GetPlayer();
-    if (!self || !self->duel)
-        return;
+	// Check for own duel info first
+	Player* self = GetPlayer();
+	if (!self || !self->duel)
+		return;
 
-    // Check if we are not accepting our own duel request
-    Player* initiator = self->duel->initiator;
-    if (!initiator || self == initiator)
-        return;
+	// Check if we are not accepting our own duel request
+	Player* initiator = self->duel->initiator;
+	if (!initiator || self == initiator)
+		return;
 
-    // Check for opponent
-    Player* opponent = self->duel->opponent;
-    if (!opponent || self == opponent)
-        return;
+	// Check for opponent
+	Player* opponent = self->duel->opponent;
+	if (!opponent || self == opponent)
+		return;
 
-    // Check if duel is starting
-    if (self->duel->startTimer != 0 || opponent->duel->startTimer != 0)
-        return;
+	// Check if duel is starting
+	if (self->duel->startTimer != 0 || opponent->duel->startTimer != 0)
+		return;
 
-    // Check if duel is in progress
-    if (self->duel->startTime != 0 || opponent->duel->startTime != 0)
-        return;
+	// Check if duel is in progress
+	if (self->duel->startTime != 0 || opponent->duel->startTime != 0)
+		return;
 
-    DEBUG_FILTER_LOG(LOG_FILTER_COMBAT, "WORLD: received CMSG_DUEL_ACCEPTED");
-    DEBUG_FILTER_LOG(LOG_FILTER_COMBAT, "Player 1 is: %u (%s)", self->GetGUIDLow(), self->GetName());
-    DEBUG_FILTER_LOG(LOG_FILTER_COMBAT, "Player 2 is: %u (%s)", opponent->GetGUIDLow(), opponent->GetName());
+	DEBUG_FILTER_LOG(LOG_FILTER_COMBAT, "WORLD: received CMSG_DUEL_ACCEPTED");
+	DEBUG_FILTER_LOG(LOG_FILTER_COMBAT, "Player 1 is: %u (%s)", self->GetGUIDLow(), self->GetName());
+	DEBUG_FILTER_LOG(LOG_FILTER_COMBAT, "Player 2 is: %u (%s)", opponent->GetGUIDLow(), opponent->GetName());
 
-    time_t now = time(nullptr);
-    self->duel->startTimer = now;
-    opponent->duel->startTimer = now;
+	time_t now = time(nullptr);
+	self->duel->startTimer = now;
+	opponent->duel->startTimer = now;
 
-    self->SendDuelCountdown(3000);
-    opponent->SendDuelCountdown(3000);
+
+
+
+	std::vector<Unit*> UnitsToReset = { self, opponent };
+
+	// Also fill pets up if they're alive
+	for (size_t i = 0; i < UnitsToReset.size(); ++i)
+		if (Pet* pet = UnitsToReset[i]->GetPet())
+			if (pet->isAlive())
+				UnitsToReset.push_back(pet);
+
+	for (auto& i : UnitsToReset)
+	{
+		// Reset health and regenerating powers
+		i->SetHealth(i->GetMaxHealth());
+		i->SetPower(POWER_MANA, i->GetMaxPower(POWER_MANA));
+		i->SetPower(POWER_ENERGY, i->GetMaxPower(POWER_ENERGY));
+
+		// Remove all cooldowns for pets
+		if (!i->IsPlayer())
+			i->RemoveAllCooldowns();
+
+		// Remove arena cooldowns if player isn't in a dungeon
+		if (!self->GetMap()->IsDungeon()) { self->RemoveArenaSpellCooldowns(); opponent->RemoveArenaSpellCooldowns(); }
+	}
+
+
+	self->SendDuelCountdown(3000);
+	opponent->SendDuelCountdown(3000);
 }
 
 void WorldSession::HandleDuelCancelledOpcode(WorldPacket& recvPacket)
 {
-    ObjectGuid guid;
-    recvPacket >> guid;
+	ObjectGuid guid;
+	recvPacket >> guid;
 
-    // Check for own duel info first
-    Player* self = GetPlayer();
-    if (!self || !self->duel)
-        return;
+	// Check for own duel info first
+	Player* self = GetPlayer();
+	if (!self || !self->duel)
+		return;
 
-    // Check for opponent
-    Player* opponent = self->duel->opponent;
-    if (!opponent)
-        return;
+	// Check for opponent
+	Player* opponent = self->duel->opponent;
+	if (!opponent)
+		return;
 
-    DEBUG_LOG("WORLD: Received opcode CMSG_DUEL_CANCELLED");
+	DEBUG_LOG("WORLD: Received opcode CMSG_DUEL_CANCELLED");
 
-    // If duel is in progress, then player surrendered in a duel using /forfeit
-    if (self->duel->startTime != 0)
-    {
-        self->CombatStopWithPets(true);
-        opponent->CombatStopWithPets(true);
-        self->CastSpell(self, 7267, TRIGGERED_OLD_TRIGGERED);    // beg
-        self->DuelComplete(DUEL_WON);
-        return;
-    }
-    // Player either discarded the duel using the "discard button" or used "/forfeit" before countdown reached 0
-    self->DuelComplete(DUEL_INTERRUPTED);
+	// If duel is in progress, then player surrendered in a duel using /forfeit
+	if (self->duel->startTime != 0)
+	{
+		self->CombatStopWithPets(true);
+		opponent->CombatStopWithPets(true);
+		self->CastSpell(self, 7267, TRIGGERED_OLD_TRIGGERED);    // beg
+		self->DuelComplete(DUEL_WON);
+		return;
+	}
+	// Player either discarded the duel using the "discard button" or used "/forfeit" before countdown reached 0
+	self->DuelComplete(DUEL_INTERRUPTED);
 }
